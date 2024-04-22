8d4dd80d6d5e314b00a183ffc4871d28a853eba1
Lucas Bresson <pandaboy816@gmail.com>
2024-04-22 15:51:52 -0500

    added Models at the end

--- api/Models/Follower.cs
+++ api/Models/Follower.cs
@@ -1,8 +1,8 @@
-namespace B.Banter
+namespace B.Banter.Models;
 
 public class Followers
 {
-    public int Id {get;set;}
-    public int PirateId {get;set;}
-    public int FollowerId {get;set;}
+    public int Id { get; set; }
+    public int PirateId { get; set; }
+    public int FollowerId { get; set; }
 }
--- api/Models/Pirate.cs
+++ api/Models/Pirate.cs
@@ -1,4 +1,4 @@
-namespace B.Banter;
+namespace B.Banter.Models;
 
 public class Pirate
 {
@@ -7,6 +7,6 @@ public class Pirate
     public int? Age { get; set; }
     public string? Nationality { get; set; }
     public string Rank { get; set; }
-    public string Ship { get ; set; }
+    public string Ship { get; set; }
     public string? Image_Url { get; set; }
 }
