129dd8f4bcf3d497e06561498698c3ae18b5293a
Kayce-Bracewell <Kayceb98@gmail.com>
2024-04-24 13:02:39 -0500

    dashboard fetch calls modified and working. There may be further changes needed..

--- api/Models/DTOs/FollowerDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace B.Banter.Models.DTOs;
-
-public class FollowersDTO
-{
-    public int Id { get; set; }
-    public int PirateId { get; set; }
-    public int FollowerId { get; set; }
-}
--- api/Models/Follower.cs
+++
@@ -1,8 +0,0 @@
-namespace B.Banter.Models;
-
-public class Followers
-{
-    public int Id { get; set; }
-    public int PirateId { get; set; }
-    public int FollowerId { get; set; }
-}
